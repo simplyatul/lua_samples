905c0635720d1e6724ef1f43c714aff1ba82b8fb
simplyatul <atulthosar@gmail.com>
2020-04-14 23:49:59 +0530

    strings...

---
+++ string_4.lua
@@ -0,0 +1,10 @@
+
+aStr = '"quote" without escape'
+print(aStr)
+aStr = "'quote' without escape"
+print(aStr)
+
+-- Both are equivalent; 
+-- the only difference is that inside each kind of quote we can use the other quote without escapes.
+-- Courtesy Programming in Lua
+
---
+++ string_5.lua
@@ -0,0 +1,6 @@
+
+aStr = '\49a'
+print(aStr)	--> 1a
+aStr = "\0490" 	--> if 2 digit ascii code is followed by number, then it has to precede by 0
+print(aStr)	--> 10 
+
