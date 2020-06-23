40ab35a55e78492d79fda0f029aa479e3c2c39ce
simplyatul <atulthosar@gmail.com>
2020-06-23 18:40:51 +0530

    - updates

--- time_and_date_0.lua
+++ time_and_date_0.lua
@@ -1,7 +1,26 @@
 -- Demonstrate OS related APIs
 -- Ref: https://www.lua.org/pil/22.1.html
 
-print("Path: ", os.getenv("PATH"))
+year=1970
+month=1
+day=1
+hour=0
+min=0
+sec=0
+wday = 'Monday'
+
+-- year = year, month = month, day = day,
+print("Time: ", os.time({ year = year, month = month, day = day, hour = hour, min = min, sec = sec }))
+
+local temp =  os.date("*t", os.time({ year = year, month = month, day = day, hour = hour, min = min, sec = sec }))
+require 'pl.pretty'.dump(temp)
+print('-----------------------')
+-- print(tostring(require 'pl.pretty'.write(temp)))
+
+print('Curr Date/Time')
+local currDate = os.date("*t", os.time())
+require 'pl.pretty'.dump(currDate)
+print(currDate, currDate.month)
 
 print("Month: ", os.date("%b"))
 print("Weekday name : ", os.date("%a"), os.date("%A"))
@@ -9,10 +28,4 @@ print("Month Digit: ", os.date("%m"))
 print("day of the month: ", os.date("%d"))
 print("Date: ", os.date("%x"))
 
-print("Time: ", os.time())
-
-local temp =  os.date("*t", os.time())
-
-require 'pl.pretty'.dump(temp)
-print('-----------------------')
-print(tostring(require 'pl.pretty'.write(temp)))
+print("Path: ", os.getenv("PATH"))
